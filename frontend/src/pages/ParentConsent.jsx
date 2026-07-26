cd829ca4b39de4117ae9a4cbfb636a021e3432b2
Deeba <deeba.raine11@gmail.com>
2026-07-25 22:51:26 -0400

    adjust folders

--- frontend/src/pages/ParentConsent.jsx
+++ frontend/src/pages/ParentConsent.jsx
@@ -1,10 +1,10 @@
-import Header from '../components/Header';
-import StudentInformation from '../components/StudentInformation';
-import VaccineHistory from '../components/VaccineHistory';
-import HealthHistory from '../components/HealthHistory';
-import ConsentForVaccination from '../components/ConsentForVaccination';
-import ConsentDeclaration from '../components/ConsentDeclaration';
-import SubmitButton from '../components/SubmitButton';
+import Header from '../components/parent/Header';
+import StudentInformation from '../components/parent/StudentInformation';
+import VaccineHistory from '../components/parent/VaccineHistory';
+import HealthHistory from '../components/parent/HealthHistory';
+import ConsentForVaccination from '../components/parent/ConsentForVaccination';
+import ConsentDeclaration from '../components/parent/ConsentDeclaration';
+import SubmitButton from '../components//parent/SubmitButton';
 
 function ParentConsent() {
     const handleSubmit = (e) => {
