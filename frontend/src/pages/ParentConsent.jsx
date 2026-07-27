53b13b45b2ca1b650346f97b976d690d59f2c4c9
Deeba <deeba.raine11@gmail.com>
2026-07-27 16:32:56 -0400

    initialize state for all components

--- frontend/src/pages/ParentConsent.jsx
+++ frontend/src/pages/ParentConsent.jsx
@@ -7,6 +7,86 @@ import ConsentDeclaration from '../components/parent/ConsentDeclaration';
 import SubmitButton from '../components//parent/SubmitButton';
 
 function ParentConsent() {
+
+    const [consentForm, setConsentFrom] = useState({
+        studentInformation: {
+            lastName: "",
+            firstName: "",
+            cardNumber: "",
+            gender: "",
+            dob: "",
+            school: "",
+            grade: "",
+            studentClass: "",
+            teacher: "",
+            guardianPhone: ""
+        },
+
+       vaccineHistory: {
+            meningococcal: {
+                received: "",
+                brand: "",
+                dateReceived: ""
+            },
+
+            hpv: {
+                received: "",
+                brand: "",
+                dateReceived: ""
+            },
+
+            hepatitisB: {
+                received: "",
+                brand: "",
+                dateReceived: ""
+            }
+        },
+
+       healthHistory: {
+            allergy: {
+                hasAllergy: "",
+                details: ""
+            },
+
+            vaccineReaction: {
+                hasReaction: "",
+                details: ""
+            },
+
+            fainting: {
+                hasHistory: "",
+                details: ""
+            },
+
+            immuneSystem: {
+                hasCondition: "",
+                details: ""
+            },
+
+            seriousMedicalCondition: {
+                hasCondition: "",
+                details: ""
+            }
+        },
+
+        consentForVaccination: {
+            meningococcal: "",
+            hpv: "",
+            hepatitisB: ""
+        },
+
+        consentDeclaration: {
+            relationshipToStudent: "",
+            parentFirstName: "",
+            parentLastName: "",
+            parentEmail: "",
+            parentPhone: "",
+            signature: "",
+            consentDate: "",
+            confirmAccuracy: false
+        }
+    });
+
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log('Form submitted');
