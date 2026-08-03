ef17e77572d94d3d46fe84a9d24795a84cb555aa
Deeba <deeba.raine11@gmail.com>
2026-08-03 17:55:47 -0400

    create and test db cnecction

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,18 @@
-const mysql = require("mysql2")
-const dotenv = require("dotenv").config()
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
-module.exports = db
+pool.query("SELECT 1")
+.then(()=> {console.log("database is connected successfully")})
+.catch(err=> {console.log("db connection faild /n" + err)})
+
+module.exports = pool;
